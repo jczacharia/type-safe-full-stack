af42fb9af6b8d18eb653600313c6a921c7cde0f3
Jeremy Zacharia <jczacharia@gmail.com>
2020-04-05 13:59:39 -0400

    initial commit

---
+++ apps/ng-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'ng-app',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/ng-app',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
